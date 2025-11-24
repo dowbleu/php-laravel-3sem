7a6d2b2bd95e7aab550e64dbd6f189436df68903
Argunov Denis <denfun31@gmail.com>
2025-11-24 22:03:01 +0300

    first lab is done

---
+++ resources/views/main/about.blade.php
@@ -0,0 +1,5 @@
+@extends('layout')
+@section('content')
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem dicta tempore accusantium? Odio, quos laudantium hic
+        assumenda quia maiores veniam! Explicabo ullam culpa nulla ab in blanditiis ducimus est. Corrupti!</p>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,19 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('layout');
+});
+
+Route::get('/about', function() {
+    return view('main.about');
+});
+
+Route::get('/contact', function() {
+    $array = [
+        'name' => 'Moscow Polytech',
+        'adress' => 'B. Semenovskaya h.38',
+        'email' => '..@mospolytech.ru',
+        'phone' => '8(499)232-2222'
+    ];
+    return view('main.contact', ['contact' => $array]);
 });
